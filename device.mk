1cbc08f82888f90fd7cf54bf4f385d5d1420fd07
CumaRull <102359107+cumaRull@users.noreply.github.com>
2024-10-18 12:35:02 +0000

    even: system only

--- device.mk
+++ device.mk
@@ -397,6 +397,7 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0.vendor \
     android.hardware.tetheroffload.control@1.0.vendor \
     android.hardware.tetheroffload.control@1.1.vendor
+    
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy 
