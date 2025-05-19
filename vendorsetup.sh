175db540bfb716de45b5416a341e1d380804e209
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-05-19 07:07:25 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 echo "Remove depencies"
 rm -rf kernel/realme/even
-rm -rf prebuilts/clang/host/linux-x86/zyc-clang
+rm -rf prebuilts/clang/host/linux-x86/mylitle-clang
 rm -rf vendor/realme/even
 echo "Cloning Dependencies"
-git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b ksu-rui2 ./kernel/realme/even
-git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git -b 18 ./prebuilts/clang/host/linux-x86/zyc-clang
+git clone --depth=1 --recurse-submodule https://github.com/yuzumi86/kernel_realme_mt6768.git -b master ./kernel/realme/even
+git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_even.git -b thirteen ./vendor/realme/even
 echo "Done!"
 
