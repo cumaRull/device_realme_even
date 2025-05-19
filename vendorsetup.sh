60fe06a250e2e2eb63f456199f6537375c067591
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-05-19 16:13:15 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,8 +3,8 @@ rm -rf kernel/realme/even
 rm -rf prebuilts/clang/host/linux-x86/mylitle-clang
 rm -rf vendor/realme/even
 echo "Cloning Dependencies"
-git clone --depth=1 --recurse-submodule https://github.com/yuzumi86/kernel_realme_even.git -b even-rmui2 ./kernel/realme/even
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b.git -b lineage-20.0 ./prebuilts/clang/host/linux-x86/mylitle-clang
+git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b ksu-rui2 ./kernel/realme/even
+git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git -b 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_even.git -b thirteen ./vendor/realme/even
 echo "Done!"
 
