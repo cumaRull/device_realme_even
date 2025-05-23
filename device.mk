c0e70fa2298a097946c0b48cdc3f7d64c06422ce
cumaRull <rulsmods@gmail.com>
2025-05-23 20:05:31 +0700

    even: add bluetooth blobs
    Signed-off-by: cumaRull <rulsmods@gmail.com>

--- device.mk
+++ device.mk
@@ -58,7 +58,8 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default
+    audio.a2dp.default \
+    audio.bluetooth.default  
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
